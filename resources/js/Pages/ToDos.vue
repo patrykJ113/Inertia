d099dbfb9dfea782fa0f947ab271679dcbef6ed9
Patryk <patryk.d.janus@gmail.com>
2023-10-24 14:26:23 +0200

    add simple toDos vue commponent

---
+++ resources/js/Pages/ToDos.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <ul v-if="toDos.length !== 0">
+            <li v-for="toDo in toDos" :key="toDo.id">
+                <h2>{{ toDo.title }}</h2>
+                <p>{{ toDo.text }}</p>
+            </li>
+        </ul>
+        <form action="">
+            <fieldset>
+                <legend>Add to do</legend>
+
+                <label>
+                    Title 
+                    <input type="text">
+                </label>
+                <label>
+                    Text 
+                    <input type="text">
+                </label>
+                <button>Add</button>
+            </fieldset>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+      props: {
+        toDos: Array
+      }  
+    }
+</script>
+
+<style>
+
+</style>
