f0261adcff15ac58515769a15c0b43fdb7945f34
Patryk <patryk.d.janus@gmail.com>
2023-10-24 11:26:51 +0200

    Basic setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-import './bootstrap';
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/vue2'
+
+createInertiaApp({
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+    return pages[`./Pages/${name}.vue`]
+  },
+  setup({ el, App, props, plugin }) {
+    Vue.use(plugin)
+
+    new Vue({
+      render: h => h(App, props),
+    }).$mount(el)
+  },
+  // Vite
+resolve: name => {
+  const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+  return pages[`./Pages/${name}.vue`]
+},
+})
