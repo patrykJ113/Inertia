9c527866a5c8c752e31b7dea0ca01726340a6b10
Patryk <patryk.d.janus@gmail.com>
2023-10-25 12:21:24 +0200

    add todo-form.vue

---
+++ resources/js/Components/todo-form.vue
@@ -0,0 +1,30 @@
+<template>
+    <form>
+        <label>
+            Title
+            <input v-model="toDo.title" type="text" />
+        </label>
+        <label>
+            Description
+            <input v-model="toDo.description" type="text" />
+        </label>
+        <button @click="addToDo">Add</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "TodoForm",
+    data() {
+        return {
+            toDo: {},
+            baseUrl: "/todo",
+        };
+    },
+    methods: {
+        addToDo() {
+            router.post(this.baseUrl, { ...this.data });
+        },
+    },
+};
+</script>
