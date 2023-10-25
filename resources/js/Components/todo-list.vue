02c99ee1b1c119c3af8e6f3db51ce34bc5594f7a
Patryk <patryk.d.janus@gmail.com>
2023-10-25 12:21:00 +0200

    add todo-list.vue

---
+++ resources/js/Components/todo-list.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul v-if="toDosArr.length !== 0" class="todo-list">
+        <TodoItem v-for="toDo in toDosArr" :key="toDo.id" :data="toDo" />
+    </ul>
+</template>
+
+<script>
+import TodoItem from "./todo-item.vue";
+
+export default {
+    name: "TodoList",
+    props: {
+        toDosArr: Array,
+    },
+    components: {
+        TodoItem
+    },
+};
+</script>
