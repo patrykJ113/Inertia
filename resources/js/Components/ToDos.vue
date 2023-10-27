ffadd71dc1e77327e1fd8021770b18b06c30b057
Patryk <patryk.d.janus@gmail.com>
2023-10-27 09:43:56 +0200

    extract tabs code to todo-tabs.vue

--- resources/js/Components/ToDos.vue
+++ resources/js/Components/ToDos.vue
@@ -2,32 +2,25 @@
     <div class="todos">
         <h2 class="todo__title">To-Do List</h2>
         <TodoForm />
-        <button @click="comp = 'TodoList'">All</button>
-        <button @click="comp = 'TodoFlagged'">Flagged</button>
-        <keep-alive>
-            <component :is="comp" />
-        </keep-alive>
+        <TodoTabs />
     </div>
 </template>
 
 <script>
 import "../../css/Components/todos.css";
 import TodoForm from "./todo-form.vue";
-import TodoList from "./todo-list.vue";
-import TodoFlagged from "./todo-flagged.vue";
+import TodoTabs from "./todo-tabs.vue";
 
 export default {
     name: "Todos",
     data() {
         return {
             baseUrl: "/todo",
-            comp: 'TodoList'
         };
     },
     components: {
         TodoForm,
-        TodoList,
-        TodoFlagged
+        TodoTabs,
     },
 };
 </script>
---
+++ resources/js/Components/todo-tabs.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="comp = 'TodoList'">All</button>
+    <button @click="comp = 'TodoFlagged'">Flagged</button>
+    <keep-alive>
+        <component :is="comp" />
+    </keep-alive>
+</template>
+
+<script>
+import TodoList from "./todo-list.vue";
+import TodoFlagged from "./todo-flagged.vue";
+export default {
+    name: 'TodoTabs',
+    data() {
+        return {
+            baseUrl: "/todo",
+            comp: "TodoList",
+        };
+    },
+    components: {
+        TodoList,
+        TodoFlagged,
+    },
+};
+</script>
