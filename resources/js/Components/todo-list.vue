e7caed485ae8ea67a58d4891eeb06a7a7d2b6bc7
Patryk <patryk.d.janus@gmail.com>
2023-10-28 12:53:17 +0200

    revert

--- resources/js/Components/todo-list.vue
+++ resources/js/Components/todo-list.vue
@@ -1,11 +1,12 @@
 <template>
-    <TransitionGroup
-        name="list"
-        tag="ul"
-        v-if="compToDos.length !== 0"
-        class="todo-list"
-    >
-        <TodoItem v-for="toDo in compToDos" :key="toDo.id" :data="toDo" />
+    <TransitionGroup name="list" tag="ul" v-if="hasToDos" class="todo-list">
+        <TodoItem
+            v-for="toDo in toDos"
+            :key="toDo.id"
+            :data="toDo"
+            :flagged="flagged"
+        />
+        
     </TransitionGroup>
     <div v-else>
         <h2>{{ noToDosText }}</h2>
@@ -15,25 +16,25 @@
 <script>
 import "../../css/Components/todo-list.css";
 import TodoItem from "./todo-item.vue";
+import useList from "../composables/useList";
 
 export default {
     name: "TodoList",
-    inject: ["toDos"],
-    data() {
-        return {
-            flagged: false
-        };
-    },
     components: {
-        TodoItem,
+        TodoItem
     },
-    computed: {
-        compToDos() {
-            return this.flagged ? this.toDos.filter((toDo) => toDo.flag) : this.toDos;
-        },
-        noToDosText() {
-            return this.flagged ? `No Flagged To-Do's Yet` : `No To-Do's Yet`
-        }
+    setup() {
+
+        const { toDos, noToDosText, hasToDos} = useList()
+        let { flagged } = useList()
+
+
+       return {
+            toDos,
+            flagged,
+            noToDosText,
+            hasToDos
+        };
     },
 };
 </script>
